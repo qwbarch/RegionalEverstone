49982540efa5420dbf72181f06cb8d62657d586b
Edward <qwbarch@github.com>
2020-07-01 23:29:13 -0400

    1.0 Snapshot

---
+++ src/main/kotlin/qwbarch/pixelmon/BreedingListener.kt
@@ -0,0 +1,28 @@
+package qwbarch.pixelmon
+
+import com.pixelmonmod.pixelmon.api.events.BreedEvent
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+
+class BreedingListener {
+
+    @SubscribeEvent
+    fun onMakeEgg(event: BreedEvent.MakeEgg) {
+        if (parentIsHoldingEverstone(event.parent1) &&
+                parentIsHoldingEverstone(event.parent2)) {
+            passRegionalFormToEgg(if ((0..1).random() == 0) event.parent1 else event.parent2, event.egg)
+        } else if (parentIsHoldingEverstone(event.parent1)) {
+            passRegionalFormToEgg(event.parent1, event.egg)
+        } else if (parentIsHoldingEverstone(event.parent2)) {
+            passRegionalFormToEgg(event.parent2, event.egg)
+        }
+    }
+
+    private fun parentIsHoldingEverstone(parent: Pokemon): Boolean {
+        return parent.heldItem.item.registryName.toString() == "pixelmon:ever_stone"
+    }
+
+    private fun passRegionalFormToEgg(parent: Pokemon, egg: Pokemon) {
+        egg.form = parent.form
+    }
+}
